d16bcc5a3e51b1394b2b81ed2aacd2b444f27ec7
Rafael Afonso <rafael.afonso@inspira.com.br>
2025-04-28 16:55:23 -0300

    esconde layout antes de uso + atualiza libs de tempo

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.example.androidplayground
 
 import android.app.DatePickerDialog
-import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
-import java.text.SimpleDateFormat
-import java.util.Locale
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 class MainActivity : AppCompatActivity() {
 
     private var tvSelectedDate: TextView? = null
     private var tvSelectedDateMinutes: TextView? = null
+    private var dataView: LinearLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         val btnSelectDate = findViewById<Button>(R.id.select_date_button)
         tvSelectedDate = findViewById(R.id.selected_date_value_str)
         tvSelectedDateMinutes = findViewById(R.id.minutes_passed_value_str)
+        dataView = findViewById(R.id.date_selected_data)
         btnSelectDate.setOnClickListener {
             pickDate()
         }
@@ -33,33 +37,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun pickDate() {
 
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val today = LocalDate.now()
 
-        DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
-            val selectedDateString = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+        DatePickerDialog(this, { _, year, month, dayOfMonth ->
+            val selectedDate = LocalDate.of(year, month + 1, dayOfMonth)
 
-            tvSelectedDate?.text = selectedDateString
+            tvSelectedDate?.text = selectedDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
 
-            val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.UK)
-            val selectedDate = simpleDateFormat.parse(selectedDateString)
-
-            val selectedDateInMs = selectedDate?.time ?: 0
-            val currentDateInMs =
-                simpleDateFormat.parse(simpleDateFormat.format(System.currentTimeMillis()))?.time
-                    ?: 0
-
-
-            val differenceInMs = currentDateInMs - selectedDateInMs
-
-            val differenceInMinutes = differenceInMs / (1000 * 60)
+            val differenceInMinutes = ChronoUnit.MINUTES.between(
+                selectedDate.atStartOfDay(ZoneId.systemDefault()),
+                today.atStartOfDay(ZoneId.systemDefault())
+            )
 
             tvSelectedDateMinutes?.text = differenceInMinutes.toString()
 
+            dataView?.visibility = View.VISIBLE
 
-        }, year, month, day).show()
+        }, today.year, today.monthValue - 1, today.dayOfMonth).show()
 
 
     }
