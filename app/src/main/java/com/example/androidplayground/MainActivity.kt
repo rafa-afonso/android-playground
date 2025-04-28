b91fb1f0bad3dd4abdf1348e041d03267b2e44de
Rafael Afonso <rafael.afonso@inspira.com.br>
2025-04-28 15:31:26 -0300

    initial layout

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -1,31 +1,48 @@
 package com.example.androidplayground
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setup()
 
-        val btnClickMe = findViewById<Button>(R.id.myButton)
-        val textView = findViewById<TextView>(R.id.counter)
-
-        var counterVal = 0
-        btnClickMe.setOnClickListener {
-            counterVal++
-            textView.text = counterVal.toString()
+        val btnSelectDate = findViewById<Button>(R.id.select_date_button)
+        btnSelectDate.setOnClickListener {
+            Toast.makeText(this, "Pressed", Toast.LENGTH_LONG).show()
         }
     }
+
+    private fun setup() {
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val mainView = findViewById<View>(R.id.main)
+
+        val originalPaddingLeft = mainView.paddingLeft
+        val originalPaddingTop = mainView.paddingTop
+        val originalPaddingRight = mainView.paddingRight
+        val originalPaddingBottom = mainView.paddingBottom
+
+        ViewCompat.setOnApplyWindowInsetsListener(mainView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            v.updatePadding(
+                left = originalPaddingLeft + systemBars.left,
+                top = originalPaddingTop + systemBars.top,
+                right = originalPaddingRight + systemBars.right,
+                bottom = originalPaddingBottom + systemBars.bottom
+            )
+            insets
+        }
+    }
+
 }
