213beb638f65538c1991cf489ca04dfd69c85f0d
Rafael Afonso <rafael.afonso@inspira.com.br>
2025-04-29 10:02:27 -0300

    Adiciona layout inicial

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -1,31 +1,40 @@
 package com.example.androidplayground
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setup()
+    }
+
+    private fun setup() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        val mainView = findViewById<View>(R.id.main)
+
+        val originalPaddingLeft = mainView.paddingLeft
+        val originalPaddingTop = mainView.paddingTop
+        val originalPaddingRight = mainView.paddingRight
+        val originalPaddingBottom = mainView.paddingBottom
+
+        ViewCompat.setOnApplyWindowInsetsListener(mainView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            v.updatePadding(
+                left = originalPaddingLeft + systemBars.left,
+                top = originalPaddingTop + systemBars.top,
+                right = originalPaddingRight + systemBars.right,
+                bottom = originalPaddingBottom + systemBars.bottom
+            )
             insets
-        }
-
-        val btnClickMe = findViewById<Button>(R.id.myButton)
-        val textView = findViewById<TextView>(R.id.counter)
-
-        var counterVal = 0
-        btnClickMe.setOnClickListener {
-            counterVal++
-            textView.text = counterVal.toString()
         }
     }
 }
