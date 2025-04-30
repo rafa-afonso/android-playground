88689f765765bb8747c913ede99f91f6c014c8f8
Rafael Afonso <rafael.afonso@inspira.com.br>
2025-04-30 15:26:25 -0300

    Adiciona viewmodels

--- app/src/main/java/com/example/androidplayground/ui/activities/news/NewsActivity.kt
+++ app/src/main/java/com/example/androidplayground/ui/activities/news/NewsActivity.kt
@@ -1,21 +1,35 @@
 package com.example.androidplayground.ui.activities.news
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.androidplayground.R
+import com.example.androidplayground.databinding.ActivityNewsBinding
+import com.example.androidplayground.db.ArticleDB
+import com.example.androidplayground.repository.NewsRepository
+import com.example.androidplayground.viewmodel.news.NewsViewModel
+import com.example.androidplayground.viewmodel.news.NewsViewModelProviderFactory
 
 class NewsActivity : AppCompatActivity() {
+
+
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var binding: ActivityNewsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_news)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val newsRepository = NewsRepository(ArticleDB(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        newsViewModel = ViewModelProvider(this, viewModelProviderFactory)[NewsViewModel::class.java]
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/androidplayground/viewmodel/news/NewsViewModel.kt
@@ -0,0 +1,136 @@
+package com.example.androidplayground.viewmodel.news
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.androidplayground.model.news.Article
+import com.example.androidplayground.model.news.Headlines
+import com.example.androidplayground.repository.NewsRepository
+import com.example.androidplayground.util.Resource
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.Response
+
+
+class NewsViewModel(app: Application, val newsRepository: NewsRepository) : AndroidViewModel(app) {
+
+    val headlines: MutableLiveData<Resource<Headlines>> = MutableLiveData()
+    private var headlinesPage = 1
+    private var headlinesResponse: Headlines? = null
+
+    val searchNews: MutableLiveData<Resource<Headlines>> = MutableLiveData()
+    var searchNewsPage = 1
+    var searchNewsResponse: Headlines? = null
+    var newSearchQuery: String? = null
+    var oldSearchQuery: String? = null
+
+    init {
+        getHeadlines("us")
+    }
+
+    fun getHeadlines(countryCode: String) = viewModelScope.launch {
+        headlinesInternet(countryCode)
+    }
+
+    fun getSearchNews(searchQuery: String) = viewModelScope.launch {
+        searchNewsInternet(searchQuery)
+    }
+
+    private fun handleHeadlinesResponse(response: Response<Headlines>): Resource<Headlines> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                headlinesPage++
+                if (headlinesResponse == null) {
+                    headlinesResponse = resultResponse
+                } else {
+                    val oldArticles = headlinesResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+                }
+                return Resource.Success(headlinesResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    private fun handleSearchNewsResponse(response: Response<Headlines>): Resource<Headlines> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                if (headlinesResponse == null || newSearchQuery != oldSearchQuery) {
+                    searchNewsPage = 1
+                    oldSearchQuery = newSearchQuery
+                    searchNewsResponse = resultResponse
+                } else {
+                    searchNewsPage++
+                    val oldArticles = searchNewsResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+                }
+                return Resource.Success(searchNewsResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    fun addToFavorites(article: Article) = viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+
+    fun getFavoriteNews() = newsRepository.getFavoriteNews()
+
+    fun deleteFromFavorites(article: Article) = viewModelScope.launch {
+        newsRepository.deleteArticle(article)
+    }
+
+    fun internetConnection(context: Context): Boolean {
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).apply {
+            return getNetworkCapabilities(activeNetwork)?.run {
+                when {
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> false
+                }
+            } ?: false
+        }
+    }
+
+    private suspend fun headlinesInternet(countryCode: String) {
+        headlines.postValue(Resource.Loading())
+        try {
+            if (internetConnection(this.getApplication())) {
+                val response = newsRepository.getHeadlines(countryCode, headlinesPage)
+                headlines.postValue(handleHeadlinesResponse(response))
+            } else {
+                headlines.postValue(Resource.Error("No internet connection"))
+            }
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> headlines.postValue(Resource.Error("Unable to connect"))
+                else -> headlines.postValue(Resource.Error("No signal"))
+            }
+        }
+    }
+
+    private suspend fun searchNewsInternet(searchQuery: String) {
+        newSearchQuery = searchQuery
+        searchNews.postValue(Resource.Loading())
+        try {
+            if (internetConnection(this.getApplication())) {
+                val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+                searchNews.postValue(handleSearchNewsResponse(response))
+            } else {
+                searchNews.postValue(Resource.Error("No internet connection"))
+            }
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> searchNews.postValue(Resource.Error("Unable to connect"))
+                else -> searchNews.postValue(Resource.Error("No signal"))
+            }
+        }
+    }
+}
