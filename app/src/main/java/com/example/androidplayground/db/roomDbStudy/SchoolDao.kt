cae55039c9cb6b24a3289a8d7ec695c6fcef63c0
Rafael Afonso <rafael.afonso@inspira.com.br>
2025-05-06 11:54:29 -0300

    Ajusta dao

--- app/src/main/java/com/example/androidplayground/db/roomDbStudy/SchoolDao.kt
+++ app/src/main/java/com/example/androidplayground/db/roomDbStudy/SchoolDao.kt
@@ -8,8 +8,12 @@ import androidx.room.Transaction
 import com.example.androidplayground.model.roomDbStudy.Director
 import com.example.androidplayground.model.roomDbStudy.School
 import com.example.androidplayground.model.roomDbStudy.Student
+import com.example.androidplayground.model.roomDbStudy.Subject
 import com.example.androidplayground.model.roomDbStudy.relations.SchoolAndDirector
 import com.example.androidplayground.model.roomDbStudy.relations.SchoolWithStudents
+import com.example.androidplayground.model.roomDbStudy.relations.StudentSubjectCrossRef
+import com.example.androidplayground.model.roomDbStudy.relations.StudentWithSubjects
+import com.example.androidplayground.model.roomDbStudy.relations.SubjectWithStudents
 
 @Dao
 interface SchoolDao {
@@ -23,6 +27,12 @@ interface SchoolDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertStudent(student: Student)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubject(subject: Subject)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStudentSubjectCrossRef(crossRef: StudentSubjectCrossRef)
+
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolAndDirectorWithSchoolName(schoolName: String): List<SchoolAndDirector>
@@ -30,5 +40,13 @@ interface SchoolDao {
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolWithStudentsWithSchoolName(schoolName: String): List<SchoolWithStudents>
+
+    @Transaction
+    @Query("SELECT * FROM subject WHERE subjectName = :subjectName")
+    suspend fun getStudentOfSubject(subjectName: String): List<SubjectWithStudents>
+
+    @Transaction
+    @Query("SELECT * FROM student WHERE studentName = :studentName")
+    suspend fun getSubjectsOfStudent(studentName: String): List<StudentWithSubjects>
 
 }
