ff330249d031e4ed16581dedd033aa6e6a04c7d9
Rafael Afonso <rafael.afonso@inspira.com.br>
2025-05-06 11:41:34 -0300

    Adiciona relacionamento 1:N

--- app/src/main/java/com/example/androidplayground/db/roomDbStudy/SchoolDao.kt
+++ app/src/main/java/com/example/androidplayground/db/roomDbStudy/SchoolDao.kt
@@ -7,7 +7,9 @@ import androidx.room.Query
 import androidx.room.Transaction
 import com.example.androidplayground.model.roomDbStudy.Director
 import com.example.androidplayground.model.roomDbStudy.School
+import com.example.androidplayground.model.roomDbStudy.Student
 import com.example.androidplayground.model.roomDbStudy.relations.SchoolAndDirector
+import com.example.androidplayground.model.roomDbStudy.relations.SchoolWithStudents
 
 @Dao
 interface SchoolDao {
@@ -18,8 +20,15 @@ interface SchoolDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertDirector(director: Director)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStudent(student: Student)
+
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolAndDirectorWithSchoolName(schoolName: String): List<SchoolAndDirector>
+
+    @Transaction
+    @Query("SELECT * FROM school WHERE schoolName = :schoolName")
+    suspend fun getSchoolWithStudentsWithSchoolName(schoolName: String): List<SchoolWithStudents>
 
 }
