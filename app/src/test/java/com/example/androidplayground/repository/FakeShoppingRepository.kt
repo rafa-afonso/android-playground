9bb0a2cf11b9da69c6e26e2d7f99dea4ebb9d1de
Rafael Afonso <rafael.afonso@inspira.com.br>
2025-05-07 10:43:25 -0300

    Adiciona test repository

---
+++ app/src/test/java/com/example/androidplayground/repository/FakeShoppingRepository.kt
@@ -0,0 +1,56 @@
+package com.example.androidplayground.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.androidplayground.model.testing.ImageResponse
+import com.example.androidplayground.model.testing.ShoppingItem
+import com.example.androidplayground.util.Resource
+
+class FakeShoppingRepository : ShoppingRepository {
+
+    private val shoppingItems = mutableListOf<ShoppingItem>()
+
+    private val observableShoppingItems = MutableLiveData<List<ShoppingItem>>(shoppingItems)
+    private val observableTotalPrice = MutableLiveData<Float>()
+
+    private var shouldReturnNetworkError = false
+
+    fun setShouldReturnNetworkError(value: Boolean) {
+        shouldReturnNetworkError = value
+    }
+
+    private fun refreshLiveData() {
+        observableShoppingItems.postValue(shoppingItems)
+        observableTotalPrice.postValue(getTotalPrice())
+    }
+
+    private fun getTotalPrice(): Float {
+        return shoppingItems.sumOf { it.price.toDouble() }.toFloat()
+    }
+
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.add(shoppingItem)
+        refreshLiveData()
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.remove(shoppingItem)
+        refreshLiveData()
+    }
+
+    override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return observableShoppingItems
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return observableTotalPrice
+    }
+
+    override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+        return if (shouldReturnNetworkError) {
+            Resource.Error("Error", null)
+        } else {
+            Resource.Success(ImageResponse(listOf(), 0, 0))
+        }
+    }
+}
