ca089026145dd1e7e59d09ba8e80c977f8fffe98
Rafael Afonso <rafael.afonso@inspira.com.br>
2025-05-08 16:42:44 -0300

    Adiciona tratamento de adição de item

--- app/src/main/java/com/example/androidplayground/ui/activities/testing/ShoppingFragmentFactory.kt
+++ app/src/main/java/com/example/androidplayground/ui/activities/testing/ShoppingFragmentFactory.kt
@@ -2,16 +2,20 @@ package com.example.androidplayground.ui.activities.testing
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
+import com.bumptech.glide.RequestManager
 import com.example.androidplayground.adapters.testing.ImageAdapter
+import com.example.androidplayground.ui.activities.testing.fragments.AddShoppingItemFragment
 import com.example.androidplayground.ui.activities.testing.fragments.ImagePickFragment
 import javax.inject.Inject
 
 class ShoppingFragmentFactory @Inject constructor(
-    private val imageAdapter: ImageAdapter
+    private val imageAdapter: ImageAdapter,
+    private val glide: RequestManager
 ) : FragmentFactory() {
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when (className) {
             ImagePickFragment::class.java.name -> ImagePickFragment(imageAdapter)
+            AddShoppingItemFragment::class.java.name -> AddShoppingItemFragment(glide)
             else -> super.instantiate(classLoader, className)
         }
     }
--- app/src/main/java/com/example/androidplayground/ui/activities/testing/fragments/AddShoppingItemFragment.kt
+++ app/src/main/java/com/example/androidplayground/ui/activities/testing/fragments/AddShoppingItemFragment.kt
@@ -5,13 +5,20 @@ import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.RequestManager
 import com.example.androidplayground.R
 import com.example.androidplayground.databinding.FragmentAddShoppingItemBinding
+import com.example.androidplayground.util.Resource
 import com.example.androidplayground.viewmodel.testing.ShoppingViewModel
+import com.google.android.material.snackbar.Snackbar
+import javax.inject.Inject
 
 
-class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
+class AddShoppingItemFragment @Inject constructor(
+    val glide: RequestManager
+) : Fragment(R.layout.fragment_add_shopping_item) {
 
     val viewModel: ShoppingViewModel by viewModels()
     lateinit var binding: FragmentAddShoppingItemBinding
@@ -19,6 +26,19 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentAddShoppingItemBinding.bind(view)
+        subscribeToObservers()
+
+        val btnAddShoppingItem = binding.btnAddShoppingItem
+        val etShoppingItemName = binding.etShoppingItemName
+        val etShoppingItemAmount = binding.etShoppingItemAmount
+        val etShoppingItemPrice = binding.etShoppingItemPrice
+        btnAddShoppingItem.setOnClickListener {
+            viewModel.insertShoppingItem(
+                etShoppingItemName.text.toString(),
+                etShoppingItemAmount.text.toString(),
+                etShoppingItemPrice.text.toString()
+            )
+        }
 
         val fabAddShoppingItem = binding.ivShoppingImage
         fabAddShoppingItem.setOnClickListener {
@@ -34,5 +54,41 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    private fun subscribeToObservers() {
+        val ivShoppingImage = binding.ivShoppingImage
+        val rootLayout = binding.root
+
+        viewModel.curImageUrl.observe(viewLifecycleOwner, Observer {
+            glide.load(it).into(ivShoppingImage)
+        })
+
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { response ->
+                when (response) {
+                    is Resource.Success<*> -> {
+                        Snackbar.make(
+                            rootLayout,
+                            "Added Shopping Item",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        findNavController().popBackStack()
+                    }
+
+                    is Resource.Error<*> -> {
+                        Snackbar.make(
+                            rootLayout,
+                            response.message ?: "An unknown error has occurred",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+
+                    is Resource.Loading<*> -> {
+                        /* NO-OP */
+                    }
+                }
+            }
+        })
     }
 }
